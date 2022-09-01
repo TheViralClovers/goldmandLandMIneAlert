ce410313813d6c629e3acd8f342136415541ece3
TheViralClovers <theviralclovers@gmail.com>
2022-09-01 23:29:48 +0530

    retries added
    script doesnt break when a network request fails

--- main.py
+++ main.py
@@ -8,34 +8,27 @@ toaster = ToastNotifier()
 with open("land_payload.json") as f:
 	land_payload = json.load(f)
 
-# curr_GMM_comm = 0
-
 raw_land_data = fetch_post_data(land_payload)["rows"][0]["data"]
 curr_GMM_bal = raw_land_data["r1_reward"]/10000
 curr_GMF_bal = raw_land_data["r2_reward"]/10000
 curr_GME_bal = raw_land_data["mr_reward"]/10000
 curr_GMD_bal = raw_land_data["gmd_reward"]/10000
 
-# curr_GMM_bal = 0
-# curr_GME_bal = 0
-# curr_GMF_bal = 0
-# curr_GMD_bal = 0.0
-# gm_token_balance = [GMM_comm,GMF_comm,GME_comm,GMD_comm]
-
-# def update_gm_token(up_token_balance):
-# 	gm_token_balance[0]=up_token_balance[0]
-# 	gm_token_balance[1]=up_token_balance[1]
-# 	gm_token_balance[2]=up_token_balance[2]
-# 	gm_token_balance[3]=up_token_balance[3]
 print(f"Current token balances are: {curr_GMM_bal} GMM, {curr_GMF_bal} GMF, {curr_GME_bal} GME, {curr_GMD_bal} GMD")
 print("Listening for mines...")
 
 while(1):
 	gm_token_price_list = fetch_gm_token_price()
 	wax_usd = fetch_wax_price()
+	while True:
+		try:
+			raw_land_data = fetch_post_data(land_payload)["rows"][0]["data"]
+			new_GMM_bal = raw_land_data["r1_reward"]/10000
+		except:
+			time.sleep(10)
+			continue
+		break
 
-	raw_land_data = fetch_post_data(land_payload)["rows"][0]["data"]
-	new_GMM_bal = raw_land_data["r1_reward"]/10000
 
 	if(curr_GMM_bal!=new_GMM_bal):
 		GMM_comm = raw_land_data["r1_reward"]/10000 - curr_GMM_bal
@@ -47,7 +40,7 @@ while(1):
 
 		toaster.show_toast(f"Someone mined in your land +${total_profit:.2f}",f"+{GMM_comm:.4f} GMM $({GMM_comm*gm_token_price_list[0]*wax_usd:.2f}) \n+{GMF_comm:.4f} GMF $({GMF_comm*gm_token_price_list[1]*wax_usd:.2f})\n+{GME_comm:.4f} GME $({GME_comm*gm_token_price_list[2]*wax_usd:.2f})\n+{GMD_comm:.4f} GMD $({GMD_comm*gm_token_price_list[3]*wax_usd:.2f})")
 		
-		print(GMM_comm,GMF_comm,GME_comm,GMD_comm)
+		print(f"{GMM_comm:.4f},{GMF_comm:.4f},{GME_comm:.4f},{GMD_comm:.4f}")
 
 		curr_GMM_bal = raw_land_data["r1_reward"]/10000
 		curr_GMF_bal = raw_land_data["r2_reward"]/10000
